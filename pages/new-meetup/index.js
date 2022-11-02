9816f41999843d5c29d0b1862961378d46a87aa6
holard <fagbolagbenga2015@gmail.com>
2022-11-02 12:08:36 +0100

    navigation styling

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,11 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import Layout from '../../components/layout/Layout';
+
 function NewMeetupPage() {
     function addMeetupHandler(enteredMeetupData) {
         console.log(enteredMeetupData)
     }
-    return  <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    return  <Layout><NewMeetupForm onAddMeetup={addMeetupHandler}/></Layout>
 }
 
 export default NewMeetupPage;
