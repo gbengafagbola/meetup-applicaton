49241ad241a65416d5fd48514aafc1b8298b881a
holard <fagbolagbenga2015@gmail.com>
2022-11-03 12:27:30 +0100

    meetup Id

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+
+function MeetupDetails() {
+    return (<MeetupDetail image='https://image.cnbcfm.com/api/v1/image/107118352-1663159290671-GettyImages-1355146458.jpg?v=1663219980&w=740&h=416&ffmt=webp' title='first meetup' address='40 arrow street' description='avenue for meetup'/>
+    )
+}
+
+export default MeetupDetails;
